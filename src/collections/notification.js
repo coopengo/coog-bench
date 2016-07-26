287ff651717153ceb250241b62680e309639b5d0
Ali Kefia <ali.kefia@coopengo.com>
2016-07-26 15:32:25 +0200

    review

--- src/collections/notification.js
+++ src/collections/notification.js
@@ -1,47 +1,50 @@
-var $               = require('jquery'),
-  Backbone          = require('backbone'),
-  NotificationView  = require('../views/notification/notification'),
-  Notification      = require('../models/notification');
+var $ = require('jquery'),
+  Backbone = require('backbone'),
+  NotificationView = require('../views/notification/notification'),
+  Notification = require('../models/notification');
 require('backbone.localstorage');
 Backbone.$ = $;
-
 var Notificator = null;
 var NotificationList = Backbone.Collection.extend({
   model: Notification,
   localStorage: new Backbone.LocalStorage('notifications-backbone'),
-
-  initialize: function() {
+  initialize: function () {
     this.listenTo(this, 'add', this.add_one);
   },
-
-  next_order: function() {
-    if (!this.length) {return 1;}
-    return this.last().get('order') + 1;
+  next_order: function () {
+    if (!this.length) {
+      return 1;
+    }
+    return this.last()
+      .get('order') + 1;
   },
-
-  new_notif: function(value, mode, time) {
-    if (value === undefined) { return; }
+  new_notif: function (value, mode, time) {
+    if (value === undefined) {
+      return;
+    }
     var attributes = {};
     attributes.mode = mode || 'info';
     attributes.value = value;
     attributes.order = this.next_order();
-    if (time !== undefined){
+    if (time !== undefined) {
       attributes.timeout = time;
     }
     this.create(attributes);
   },
-
-  add_one: function(model) {
-    var view = new NotificationView({model: model});
-    $('#notification-area').append(view.render().el);
+  add_one: function (model) {
+    var view = new NotificationView({
+      model: model
+    });
+    $('#notification-area')
+      .append(view.render()
+        .el);
   },
-
   comparator: 'order'
 });
-
 if (Notificator) {
   module.exports = Notificator;
-} else {
+}
+else {
   Notificator = new NotificationList();
   module.exports = Notificator;
 }
---
+++ src/models/login.js
@@ -0,0 +1,15 @@
+//[Model]   LoginModel
+var $ = require('jquery'),
+  Backbone = require('backbone');
+Backbone.$ = $;
+module.exports = Backbone.Model.extend({
+  defaults: function () {
+    return {
+      name: '',
+      value: '',
+      valid: true,
+      type: 'text',
+      filled: false,
+    };
+  },
+});
